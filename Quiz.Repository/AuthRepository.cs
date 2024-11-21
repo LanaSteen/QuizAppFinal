b2f8e48550486f48861128a4ed1f3893cfa2427b
LanaSteen <l4n.steen@gmail.com>
2024-11-21 23:18:23 +0400

    pass correct cheak added

--- Quiz.Repository/AuthRepository.cs
+++ Quiz.Repository/AuthRepository.cs
@@ -1,11 +1,10 @@
 ﻿using Quiz.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
-using System.Xml;
 
 namespace Quiz.Repository
 {
@@ -16,51 +15,52 @@ namespace Quiz.Repository
 
         public AuthRepository(string dataFolder)
         {
-      
             _filePath = Path.Combine(dataFolder, FileName);
         }
 
-      
-        public void SaveAuthUser(AuthUser user)
+        public void SaveAuthUser(AuthUser user, string password)
         {
             var users = LoadAuthUsers();
 
-            if (users.Any())
-            {
-                
-                AuthUser lastUser = users.OrderByDescending(u => u.UserId).FirstOrDefault();
-                user.UserId = lastUser != null ? lastUser.UserId + 1 : 1;
-            }
-            else
-            {
-              
-                user.UserId = 1;
-            }
-
-      
-            foreach (var u in users)
-            {
-                u.ActiveNow = false;
-            }
-
-        
-            var existingUser = users.FirstOrDefault(u => u.Username == user.Username && u.PasswordHash == user.PasswordHash);
+            var existingUser = users.FirstOrDefault(u => u.Username == user.Username);
 
             if (existingUser != null)
             {
-              
-                existingUser.LastLogin = DateTime.Now;  
-                existingUser.ActiveNow = true;          
+                if (existingUser.PasswordHash == password)
+                {
+                    existingUser.LastLogin = DateTime.Now;
+                    existingUser.ActiveNow = true;
+                }
+                else
+                {
+                    return;
+                }
             }
             else
             {
-      
+                if (users.Any())
+                {
+                    var lastUser = users.OrderByDescending(u => u.UserId).FirstOrDefault();
+                    user.UserId = lastUser != null ? lastUser.UserId + 1 : 1;
+                }
+                else
+                {
+                    user.UserId = 1;
+                }
+
                 user.ActiveNow = true;
                 user.LastLogin = DateTime.Now;
                 users.Add(user);
             }
 
-         
+            foreach (var u in users)
+            {
+                if (u.UserId != user.UserId)
+                {
+                    u.ActiveNow = false;
+                }
+            }
+
             using (var fileStream = new FileStream(_filePath, FileMode.Create, FileAccess.Write))
             using (var writer = new StreamWriter(fileStream, Encoding.UTF8))
             {
@@ -69,12 +69,10 @@ namespace Quiz.Repository
             }
         }
 
-        
         public List<AuthUser> LoadAuthUsers()
         {
             if (!File.Exists(_filePath))
             {
-       
                 return new List<AuthUser>();
             }
 
@@ -86,13 +84,10 @@ namespace Quiz.Repository
             }
         }
 
-  
         public AuthUser GetAuthUserByUsername(string username)
         {
             var users = LoadAuthUsers();
             return users.FirstOrDefault(u => u.Username == username);
         }
     }
-
-
 }
--- QuizApp/Program.cs
+++ QuizApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Quiz.Models;
 using Quiz.Repository;
+using System;
 
 namespace QuizApp
 {
@@ -7,44 +8,43 @@ namespace QuizApp
     {
         static void Main(string[] args)
         {
-            // Define the directory where your data is stored
-            string dataFolder = @"C:\Users\l4nst\source\repos\QuizAppFinal\Quiz.Repository\Data\"; // Adjust this path if necessary
-            Directory.CreateDirectory(dataFolder); // Ensure the directory exists
+            string dataFolder = @"C:\Users\l4nst\source\repos\QuizAppFinal\Quiz.Repository\Data\";
+            Directory.CreateDirectory(dataFolder);
 
             var authRepo = new AuthRepository(dataFolder);
 
-            // Read the username and password from the console
             Console.WriteLine("Please enter your username:");
             string username = Console.ReadLine();
 
             Console.WriteLine("Please enter your password:");
-            string password = Console.ReadLine(); // In a real-world app, you'd hash this password
+            string password = Console.ReadLine();
 
-            // Create an AuthUser object with the entered data
             var loginUser = new AuthUser
             {
                 Username = username,
-                PasswordHash = password // In practice, you should hash the password
+                PasswordHash = password
             };
 
-            // Check if the user already exists
             var existingUser = authRepo.GetAuthUserByUsername(username);
 
             if (existingUser != null)
             {
-                // If the user exists, it's a re-login
-                Console.WriteLine($"{existingUser.Username} - WELCOME BACK!");
+                if (existingUser.PasswordHash == password)
+                {
+                    Console.WriteLine($"{existingUser.Username} - WELCOME BACK!");
+                    authRepo.SaveAuthUser(existingUser, password);
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect password! Please try again.");
+                }
             }
             else
             {
-                // If the user doesn't exist, it's a new log-in
                 Console.WriteLine($"{loginUser.Username} - Nice to see you in our game!");
+                authRepo.SaveAuthUser(loginUser, password);
             }
 
-            // Save the user (create a new one or update the existing one)
-            authRepo.SaveAuthUser(loginUser);
-
-            // Optionally, display all users to verify ActiveNow status
             var allUsers = authRepo.LoadAuthUsers();
             Console.WriteLine("All users in the system:");
             foreach (var u in allUsers)
@@ -56,5 +56,4 @@ namespace QuizApp
             Console.ReadKey();
         }
     }
-
 }
